e957ce4823759b9f1e20d0ac4241894a400e2f50
Yosra Gallah <gallahyosra@gmail.com>
2022-04-04 01:26:02 +0100

    sign in method+ UI

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -1,11 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 import 'package:save_password/config/size_config.dart';
 import 'package:save_password/providers/auth_provider.dart';
 import 'package:save_password/views/home_screen.dart';
-class SignIn extends StatelessWidget {
+import 'package:save_password/widgets/login_button.dart';
+import 'package:save_password/widgets/stagger_animation_login.dart';
+class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin{
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => HomeScreen()));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +42,7 @@ class SignIn extends StatelessWidget {
     var tm=SizeConfig.textMultiplier;
     var provider= context.read<AuthProvider>();
     var Dprovider=context.watch<AuthProvider>();
+    timeDilation = 1;
     return  StreamBuilder(
       stream:  FirebaseAuth.instance.authStateChanges(),
       builder: (context,snapshot) {
@@ -24,24 +53,40 @@ class SignIn extends StatelessWidget {
         }else if(snapshot.hasData) {
           return const HomeScreen();
          }else {
-          return !Dprovider.isLoading? OutlinedButton.icon(
-          icon: Image.asset("assets/icons/google.png",width: wm*7,),
-          onPressed: () async {
-           context.read<AuthProvider>().googleLogin();
-          },
-          label: const Text(
-            "Sign in",
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-          style: ButtonStyle(
-              backgroundColor:
-              MaterialStateProperty.all<Color>(Colors.grey),
-              side: MaterialStateProperty.all<BorderSide>(BorderSide.none)),
-        ) : const CircularProgressIndicator();
+          return !Dprovider.isLoading? Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/icons/login_bg.jpg"),
+                      fit: BoxFit.fill)),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Column(
+                        children:  [
+                          Padding(
+                            padding: EdgeInsets.only(top: hm*6, bottom: hm*5),
+                            child: Text("Save Passwords",style: TextStyle(fontWeight: FontWeight.bold,fontSize: tm*4,color: Colors.white),)
+                          ),
+                          SizedBox(height: hm*89,)
+                        ],
+                      ),
+                      StaggerAnimationLogin(
+                          controller:
+                          _animationController // _animationController.view
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ) : const CircularProgressIndicator();
         }
         }
     );
   }
-  }
+}
 
